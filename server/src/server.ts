98e6af65bc4b8ceca2bd3669628a144c7cc12eb9
Samuka <samuel.eugenio@aurum.com.br>
2022-07-01 21:43:33 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { routes } from './routes'
-
+var cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
